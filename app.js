edb0f28bcd8de71b120136ed5b76d7a6da8df0f0
GautamDhiman <gautamCodesHere@gmail.com>
2021-10-11 18:55:51 +0530

    done with initial setup

---
+++ app.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+
+// async errors
+
+const express = require('express');
+const app = express();
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+const notFound = require('./middleware/not-found');
+const connectDB = require('./db/storeDb');
+
+app.use(express.json());
+
+// routes
+
+app.get('/', (req, res) => {
+    res.send('<h1>Store Api</h1> <a href="/api/v1/products">products route</a>');
+})
+
+// products route
+
+app.use(notFound);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 80
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () => {
+            console.log('connected to server...');
+        })
+    } catch (err) {
+        
+    }
+}
+
+start();
