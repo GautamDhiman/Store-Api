5e4ad4282640d65fa2128d113519b12c28e5211b
GautamDhiman <gautamCodesHere@gmail.com>
2021-10-22 17:03:53 +0530

    added sort and paging capability to APIs

--- controllers/Store-Api.js
+++ controllers/Store-Api.js
@@ -1,13 +1,13 @@
 const Product = require('../models/storeApi');
 
 const getAllProductsStatic = async (req, res) => {
-    throw new Error('testing async error');
-    res.status(200).json({ msg: 'products testing route'});
+    const products = await Product.find({}).sort('-name price')
+    res.status(200).json({ products });
 }
 
 const getAllProducts = async (req, res) => {
     
-    const { featured, company, name } = req.query;
+    const { featured, company, name, sort, fields } = req.query;
     const queryObject = {};
 
     if(featured)
@@ -25,7 +25,35 @@ const getAllProducts = async (req, res) => {
         queryObject.name = { $regex: name, $options: 'i'};
     }
 
-    const products = await Product.find(queryObject);
+    let result = Product.find(queryObject);
+
+    if(sort)
+    {
+        const sortList = sort.split(',').join(' ');
+        // console.log(sortList);
+        // console.log(sort);
+        result = result.sort(sortList)
+    }
+    else
+    {
+        result = result.sort('createAt');
+    }
+
+    if(fields)
+    {
+        const fieldsList = fields.split(',').join(' ');
+        // console.log(sortList);
+        // console.log(sort);
+        result = result.select(fieldsList);
+    }
+
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page-1)*limit;
+
+    result = result.skip(skip).limit(limit);
+
+    const products = await result;
     res.status(200).json({products , nbHits: products.length});
 }
 
